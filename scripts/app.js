39a095904c7661c0304b08bb81caaf282dbe7925
Eliza Thompson <Eliza_thompson@icloud.com>
2020-07-19 15:10:27 +0100

    grids made, attempting ship making

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,57 @@
-console.log('hello world')
+function init() {
+
+  // * DOM elements 
+
+  const grids = document.querySelectorAll('.grid')
+  const cellsGrid = []
+  const ships = document.querySelectorAll('.ship')
+  const cellsShip = []
+
+  // * grid variables 
+
+  const width = 10
+  const cellCount = width * width 
+
+  // * Creating the grids
+
+  function createGrid() {
+    grids.forEach(grid => {
+      for (let i = 0; i < cellCount; i++) {
+        const cell = document.createElement('div')
+        cell.textContent = i
+        grid.appendChild(cell)
+        cellsGrid.push(cell)
+      }
+    })
+  }
+
+  createGrid()
+
+  // * ship variables
+  
+
+
+  // * create ships 
+
+  function createShips() {
+    ships.forEach(ship => {
+      for (let i = 0; i < 3; i++) {
+        const cell = document.createElement('div')
+        cell.textContent = i
+        ship.appendChild(cell)
+        cellsShip.push(cell)
+      }
+    })
+  }
+
+  createShips() 
+
+
+
+
+
+
+}
+
+
+window.addEventListener('DOMContentLoaded', init)
